eb86a8fcc238f530cf236b4a02c1853eaa72e00d
Homo-Faber-Fa <133135868+Homo-Faber-Fa@users.noreply.github.com>
2024-04-20 17:03:06 +0300

    Update MainNoStream20.04.2024.java

--- src/MainNoStream.java
+++ src/MainNoStream.java
@@ -4,17 +4,13 @@ import java.util.Collections;
 import java.util.List;
 
 public class MainNoStream {
-    // Задача №1 ДЗ 2
+    // Задача №1 ДЗ 2 (поправил 20.04.2024)
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
         List<Integer> listCopy = new ArrayList<>(list);
         for (int i = listCopy.size() - 1; i >= 0; i--) {
-            if (list.get(i) <= 0) {
+            if (list.get(i) <= 0 || list.get(i) % 2 != 0) {
                 listCopy.remove(i);
-            } else {
-                if (list.get(i) % 2 != 0) {
-                    listCopy.remove(i);
-                }
             }
         }
         list = listCopy;
@@ -22,3 +18,5 @@ public class MainNoStream {
         System.out.println(list);
     }
 }
+
+
